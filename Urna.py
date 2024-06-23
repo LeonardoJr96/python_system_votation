686e130286a145aae8872fa2459c76edd00af511
Leonardo de Almeida Junior <128478878+LeonardoJr96@users.noreply.github.com>
2024-06-23 01:01:54 -0300

    update commit: add percentual and rm function name

--- Urna.py
+++ Urna.py
@@ -1,46 +1,60 @@
+#arrumar porcentagem
+#adicionar time para cada validação   
 
-def soma_ou_Troca(numeroVotos, opt, concluido):
+def soma_ou_Troca(numeroVotos, opt):
     print(f"Confirma o candidato selecionado: (1)Sim (2)Não")
     confirma = int(input())
-    
+            
     if confirma == 1:
-        numeroVotos[int(opt)] += 1
-        concluido = True
-        return concluido
+        match opt:
+            case 10:
+                opt = 1
+                numeroVotos[opt] += 1
+            case 20:
+                opt = 2
+                numeroVotos[opt] += 1
+            case 30:
+                opt = 3
+                numeroVotos[opt] += 1
+            
+        return print('Voto válidado'), True
+    
     else:
-        concluido = False
-        return concluido
+        return False
 
-def opcao(candidatos, opt, numeroVotos, concluido):
-    opt = opt.strip()
+def opcao(candidatos, opt, numeroVotos):
     
     if opt in candidatos:
         print(candidatos[opt])
-        return soma_ou_Troca(numeroVotos, opt, concluido)
+        return soma_ou_Troca(numeroVotos, opt)
     else:
         print("Voto nulo")
         numeroVotos[4] += 1
-        return False
+        return True
 
-def exibirResultado(candidatos, numeroVotos, povo):
+def exibirResultado(candidatos, numeroVotos, quantidadePessoas):
     numeroVotos.append(numeroVotos[0] + numeroVotos[4])
-    total_votos = len(povo)
+    total_votos = quantidadePessoas
     metade_votos = total_votos / 2
+    multiplicador = 10
     
     if numeroVotos[5] >= metade_votos:
-        print("Nova votação necessária (votos em branco/nulos superam metade dos votos válidos)")
+        print("Nova votação necessária (votos em branco/nulos é igual ou supera metade dos votos válidos)")
     else:
-        for opt, votos in enumerate(numeroVotos[1:], start=1):
+        print(f'Eleitores: {quantidadePessoas}')
+        for i in range(4):
+            porcentagem = (numeroVotos[i]*100)/total_votos
+            if i == 0:
+                print(f'Votos em branco: {numeroVotos[0]} {(numeroVotos[0]*100)/total_votos}%\nVotos nulos: {numeroVotos[4]} {(numeroVotos[0]*100)/total_votos}%')
+            elif i > 0 and i <=3:
+                print(f"{candidatos[(i)*multiplicador]}: {numeroVotos[i]}  {porcentagem}%")
+                    
+        for opt, votos in enumerate(numeroVotos[1:], start=1):       
             if votos >= metade_votos:
-                for i in range(3):
-                    print(f'{candidatos[i+1]} : {numeroVotos[i+1]} votos')
-                    if i == 3:
-                        (f'{candidatos[i+2]} : {numeroVotos[i+2]} votos')
-                print(f"{candidatos[str(opt)]} venceu a eleição com {votos} votos.")
-
+                print(f"{candidatos[opt*multiplicador]} venceu a eleição com {votos} votos. Obteve {(numeroVotos[i]*100)/total_votos}% de aprovação") #arrumar porcentagem
+    
 def votacao():
-    candidatos = {"0": "Voto em Branco", "1": "leonardo", "2": "jao", "3": "romar"}
-    povo = {}
+    candidatos = {0: "Voto em Branco", 10: "Antônio da Silva", 20: "José Nascimento", 30: "Paulo Cardos"}
     numeroVotos = [0, 0, 0, 0, 0]
     
     concluido = False
@@ -48,16 +62,19 @@ def votacao():
     print("Quantas pessoas irão votar?")
     quantidadePessoas = int(input())
     
-    for i in range(quantidadePessoas):
-        print('Digite seu nome: ')
-        povo[i] = input()
     
-    while concluido == False:
-        for _ in range(quantidadePessoas):
-            print(f"{povo[_]} digite o código ou o nome do candidato:")
-            opt = input().strip()
-            opcao(candidatos, opt, numeroVotos, concluido)
+    for _ in range(quantidadePessoas):
+        concluido = False
+        while not concluido:
+            
+            for num, name in candidatos.items():
+                print(f'{num}: {name}')
+
+            print("digite o código do candidato:")
+            opt = int(input())  
+                
+            concluido = opcao(candidatos, opt, numeroVotos)
     
-    exibirResultado(candidatos, numeroVotos, povo)
+    exibirResultado(candidatos, numeroVotos, quantidadePessoas)
 
 votacao()
