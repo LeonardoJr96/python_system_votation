85fc929867435dff958547e2f00de7b6753d0764
Leonardo de Almeida Junior <128478878+LeonardoJr96@users.noreply.github.com>
2024-06-22 22:36:56 -0300

    update commit: new ajusts in logic program

--- Urna.py
+++ Urna.py
@@ -1,49 +1,63 @@
 
-def soma_ou_Troca(numeroVotos, opt):
-    while True:
-        print("Confirma o candidato selecionado: (1)Sim (2)Não")
-        confirma = int(input())
+def soma_ou_Troca(numeroVotos, opt, concluido):
+    print(f"Confirma o candidato selecionado: (1)Sim (2)Não")
+    confirma = int(input())
     
-        if confirma == 1:
-            numeroVotos[int(opt)] = numeroVotos[int(opt)] + 1
-            break
-        else:
-             return True
+    if confirma == 1:
+        numeroVotos[int(opt)] += 1
+        concluido = True
+        return concluido
+    else:
+        concluido = False
+        return concluido
 
-def opcao(candidatos, opt, numeroVotos):
-    opt.strip()
+def opcao(candidatos, opt, numeroVotos, concluido):
+    opt = opt.strip()
     
     if opt in candidatos:
         print(candidatos[opt])
-        return soma_ou_Troca(numeroVotos, opt)
+        return soma_ou_Troca(numeroVotos, opt, concluido)
     else:
-        opt = 5
         print("Voto nulo")
-        return soma_ou_Troca(numeroVotos, opt)
+        numeroVotos[4] += 1
+        return False
 
-def nomearCandidato(num, participantes):
-    for i in range(num):
-        participantes[i] = i
-    return participantes
-
-def exibirResultado(participantes, candidatos, numeroVotos):
-    print("digite o código ou o nome do candidato")
-    opt = str(input())
+def exibirResultado(candidatos, numeroVotos, povo):
+    numeroVotos.append(numeroVotos[0] + numeroVotos[4])
+    total_votos = len(povo)
+    metade_votos = total_votos / 2
     
-    
-    opcao(candidatos, opt, numeroVotos)
+    if numeroVotos[5] >= metade_votos:
+        print("Nova votação necessária (votos em branco/nulos superam metade dos votos válidos)")
+    else:
+        for opt, votos in enumerate(numeroVotos[1:], start=1):
+            if votos >= metade_votos:
+                for i in range(3):
+                    print(f'{candidatos[i+1]} : {numeroVotos[i+1]} votos')
+                    if i == 3:
+                        (f'{candidatos[i+2]} : {numeroVotos[i+2]} votos')
+                print(f"{candidatos[str(opt)]} venceu a eleição com {votos} votos.")
 
 def votacao():
-    candidatos = {"0": "Voto em Branco", "1": "romar", "2": "romar", "3": "romar"}
-    participantes = {}
+    candidatos = {"0": "Voto em Branco", "1": "leonardo", "2": "jao", "3": "romar"}
+    povo = {}
     numeroVotos = [0, 0, 0, 0, 0]
+    
+    concluido = False
     
     print("Quantas pessoas irão votar?")
     quantidadePessoas = int(input())
     
-    nomearCandidato(quantidadePessoas, participantes)
+    for i in range(quantidadePessoas):
+        print('Digite seu nome: ')
+        povo[i] = input()
     
-    exibirResultado(participantes, candidatos, numeroVotos)
-
+    while concluido == False:
+        for _ in range(quantidadePessoas):
+            print(f"{povo[_]} digite o código ou o nome do candidato:")
+            opt = input().strip()
+            opcao(candidatos, opt, numeroVotos, concluido)
+    
+    exibirResultado(candidatos, numeroVotos, povo)
 
 votacao()
